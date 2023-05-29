b377108d387b678dc889f5186c318cee1b8e2364
DaniSM108 <130647178+DaniSM108@users.noreply.github.com>
2023-05-29 08:21:49 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,14 @@
+let tool = 0
+input.onGesture(Gesture.Shake, function () {
+    tool = randint(1, 3)
+    if (true) {
+    	
+    } else if (false) {
+    	
+    } else {
+    	
+    }
+})
+basic.forever(function () {
+	
+})
